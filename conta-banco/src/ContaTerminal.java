1d52e0a69efa269cb9bfa5a6c1cb6290b7a48b87
Nandolim3 <nandolim3@gmail.com>
2023-05-26 18:24:11 -0300

    Projeto Conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in); 
+
+        System.out.print("Informe numero da sua conta: ");
+        int numero = sc.nextInt();
+        System.out.println();
+        System.out.print("Informe o numero da agência: ");
+        sc.nextLine();
+        String agencia = sc.nextLine();
+        System.out.print("Informe Seu nome: ");
+        String nomeCliente = sc.nextLine();
+        System.out.print("Informe seu Saldo: ");
+        Double saldo = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("'Olá "+ nomeCliente 
+                                  + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                                  + agencia 
+                                  +", conta " 
+                                  + numero
+                                  +" e seu saldo "
+                                  + saldo
+                                  + " já está disponível para saque.'");       
+
+        sc.close();
     }
 }
